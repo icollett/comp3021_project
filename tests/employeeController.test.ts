3be016f242b362031628d343efb6cdaff08b97a4
icollett <icollett@academic.rrc.ca>
2025-01-24 17:39:22 -0600

    Feature: Created employeeController.test.ts. Added test for getAllEmployees route.

---
+++ tests/employeeController.test.ts
@@ -0,0 +1,40 @@
+import { Request, Response, NextFunction } from "express";
+import * as employeeController from "../src/api/v1/controllers/employeeController";
+import * as employeeService from "../src/api/v1/services/employeeService";
+
+jest.mock("../src/api/v1/services/employeeService");
+
+describe("Item Controller", () => {
+    let mockReq: Partial<Request>;
+    let mockRes: Partial<Response>;
+    let mockNext: NextFunction;
+
+    beforeEach(() => {
+        jest.clearAllMocks();
+        mockReq = { params: {}, body: {} };
+        mockRes = { status: jest.fn().mockReturnThis(), json: jest.fn() };
+        mockNext = jest.fn();
+    });
+
+    describe("getAllEmployees", () => {
+        it("should handle successful operation", async () => {
+            const mockItems = [
+                { name: "Test Item", position: "Branch Manager", department: "Management", email: "alice.johnson@pixell-river.com", phone: "604-555-0148", branchID: "1"},
+            ];
+
+            (employeeService.getAllEmployees as jest.Mock).mockResolvedValue(mockItems);
+
+            await employeeController.getAllEmployees(
+                mockReq as Request,
+                mockRes as Response,
+                mockNext
+            );
+
+            expect(mockRes.status).toHaveBeenCalledWith(200);
+            expect(mockRes.json).toHaveBeenCalledWith({
+                message: "Employees Retrieved",
+                data: mockItems,
+            });
+        });
+    });
+});
