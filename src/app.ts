5e31fc505641cd69b001e10a431b65d6ae58729b
icollett <icollett@academic.rrc.ca>
2025-01-24 13:05:22 -0600

    Path: Added app health route to app.ts

--- src/app.ts
+++ src/app.ts
@@ -19,6 +19,27 @@ app.get("/", (req, res) => {
   res.send("Hello, world!");
 });
 
+// define GET route for health check
+/**
+ * @openapi
+ * /api/v1/health:
+ *  get:
+ *   summary: Get health status of the application
+ *   tags: [Health]
+ *   responses:
+ *    200:
+ *     description: The application's status, uptime, the current timestamp, and version
+ */
+app.get("/api/v1/health", (req, res) => {
+  res.json({
+    status: "OK",
+    uptime: process.uptime(),
+    timestamp: new Date().toISOString(),
+    version: "1.0.0",
+  });
+  // send JSON response with status, server uptime, current time, API version
+});
+
 // initialize port as either string read from .env, or 3000 by default
 const PORT: string | 3000 = process.env.PORT || 3000;
 
