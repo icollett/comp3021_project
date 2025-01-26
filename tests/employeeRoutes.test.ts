82da6e836b465804ede5c3010d1f221ad507202a
icollett <icollett@academic.rrc.ca>
2025-01-26 14:36:07 -0600

    Test: Added employeeRoutes testing.

---
+++ tests/employeeRoutes.test.ts
@@ -0,0 +1,38 @@
+import request from "supertest";
+import {app, server} from "../src/app";
+import * as employeeController from "../src/api/v1/controllers/employeeController";
+
+jest.mock("../src/api/v1/controllers/employeeController", () => ({
+	getAllEmployees: jest.fn((req, res) => res.status(200).send()),
+	createEmployee: jest.fn((req, res) => res.status(201).send()),
+	updateEmployee: jest.fn((req, res) => res.status(200).send()),
+	deleteEmployee: jest.fn((req, res) => res.status(200).send()),
+}));
+
+describe("Employee API Endpoints", () => {
+	it("should call createEmployee controller", async () => {
+        const mockItem = {name: "Alice Johnson", position: "Branch Manager", department: "Management", email: "alice.johnson@pixell-river.com", phone: "604-555-0148", branchID: "1"};
+        await request(app).post("/api/v1/employees").send(mockItem);
+        expect(employeeController.createEmployee).toHaveBeenCalled();
+    });
+
+    it("should call getAllEmployees controller", async () => {
+        await request(app).get("/api/v1/employees");
+        expect(employeeController.getAllEmployees).toHaveBeenCalled();
+    });
+
+    it("should call updateEmployee controller", async () => {
+        const mockItem = {name: "Alice Jackson", branchID: "3"};
+        await request(app).put("/api/v1/employees/1").send(mockItem);
+        expect(employeeController.updateEmployee).toHaveBeenCalled();
+    });
+
+    it("should call deleteEmployee controller", async () => {
+        await request(app).delete("/api/v1/employees/1");
+        expect(employeeController.deleteEmployee).toHaveBeenCalled();
+    });
+});
+
+afterAll(async () => {
+    await server.close();
+});
